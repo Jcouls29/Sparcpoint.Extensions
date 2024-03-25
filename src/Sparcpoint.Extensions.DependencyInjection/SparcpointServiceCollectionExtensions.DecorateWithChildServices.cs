2ba5e894bef875e20893359ad57aae5829adebee
Justin Coulston <justin.coulston@gmail.com>
2024-03-24 21:13:11 -0400

    Added DecorateWithChildServices

---
+++ src/Sparcpoint.Extensions.DependencyInjection/SparcpointServiceCollectionExtensions.DecorateWithChildServices.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sparcpoint.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static partial class SparcpointServiceCollectionExtensions
+{
+    public static IServiceCollection DecorateWithChildServices<TService, TDecoration>(this IServiceCollection services, Action<IServiceCollection> configure, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+    {
+        return DecorateWithChildServices(services, typeof(TService), typeof(TDecoration), configure, lifetime);
+    }
+
+    private static IServiceCollection DecorateWithChildServices(this IServiceCollection services, Type serviceType, Type implementationType, Action<IServiceCollection> configure, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+    {
+        if (serviceType.IsGenericTypeDefinition)
+            throw new InvalidOperationException("Open generics are not supported.");
+
+        bool hasBeenRouted = false;
+
+        // NOTE: This approach allows for many nested coalesced providers.
+        //       This should not cause any issues.
+        var childServices = new ServiceCollection();
+        OwnedProvider childProvider = new OwnedProvider();
+
+        Func<IServiceProvider, object, object> factory = (IServiceProvider provider, object decoratedInstance) =>
+        {
+            if (!hasBeenRouted)
+            {
+                // NOTE: Creates a route to the original registration
+                //       Uses transient scope to defer to the parent's
+                //       scope instead
+                foreach (var service in services.Reverse())
+                {
+                    if (service.ServiceType.ContainsGenericParameters && service.ImplementationType != null)
+                    {
+                        childServices.Insert(0, new ServiceDescriptor(service.ServiceType, service.ImplementationType, ServiceLifetime.Transient));
+                    }
+                    else
+                    {
+                        childServices.Insert(0, new ServiceDescriptor(service.ServiceType, (IServiceProvider p) => provider.GetRequiredService(service.ServiceType), ServiceLifetime.Transient));
+                    }
+                }
+
+                configure(childServices);
+                childProvider = new OwnedProvider(childServices.BuildServiceProvider());
+
+                // Should only be done once!! This prevents multiple initializations
+                hasBeenRouted = true;
+            }
+
+            // NOTE: Order is important here. We want to check the child
+            //       provider before the parent
+            if (childProvider.IsDisposed)
+                throw new InvalidOperationException("Child provider has been disposed.");
+
+            IServiceProvider coalescedProvider;
+            coalescedProvider = new CoalescedServiceProvider(childProvider, provider);
+
+            return ActivatorUtilities.CreateInstance(coalescedProvider, implementationType, decoratedInstance);
+        };
+
+        for (var i = 0; i < services.Count; i++)
+        {
+            if (services[i].ServiceType == serviceType)
+            {
+                var descriptor = services[i];
+                if (descriptor.ServiceType is DecoratedType)
+                    continue;
+                var decoratedTypeEx = new DecoratedType(descriptor.ServiceType);
+
+                // TODO: DecorateFactory
+                var decorateFactory = (IServiceProvider sp) =>
+                {
+                    var instance = sp.GetRequiredService(decoratedTypeEx);
+                    return factory(sp, instance);
+                };
+
+                services.Add(descriptor.WithServiceType(decoratedTypeEx));
+                services[i] = descriptor.WithImplementationFactory(decorateFactory);
+            }
+        }
+
+        // NOTE: The provider is added to the collection to allow
+        //       for the lifetime to be handled by the host (i.e. ASP.NET Core)
+        // NOTE: The factory is required to ensure it gets disposed.
+        //       Using the instance directly won't dispose at the end of the parent's life.
+
+        services.AddSingleton(p => childProvider);
+
+        return services;
+    }
+}
---
+++ test/Sparcpoint.Extensions.Tests/DependencyInjection/ServiceCollectionExtensions_DecorateWithChildServices.cs
@@ -0,0 +1,79 @@
+﻿namespace Sparcpoint.Extensions.Tests;
+
+public class ServiceCollectionExtensions_DecorateWithChildServices
+{
+    /* Scenario:
+     * Decorated class will pick up parent container service
+     * if registered
+     */
+    [Fact]
+    public void When_child_and_parent_provide_child_instance_is_used()
+    {
+        using var provider = Build(s =>
+        {
+            s.AddSingleton<IService, BaseService>();
+            s.AddSingleton(new OtherService("Parent"));
+            s.DecorateWithChildServices<IService, DecoratedService>(child =>
+            {
+                child.AddSingleton(new OtherService("Child"));
+            });
+        });
+
+        var service = provider.GetService<IService>();
+        Assert.NotNull(service);
+        var decoratedService = Assert.IsType<DecoratedService>(service);
+        Assert.IsType<BaseService>(decoratedService.Service);
+        var otherService = Assert.IsType<OtherService>(decoratedService.Other);
+        Assert.Equal("Child", otherService.Value);
+    }
+
+    [Fact]
+    public void When_child_provider_missing_service_pulls_from_parent()
+    {
+        using var provider = Build(s =>
+        {
+            s.AddSingleton<IService, BaseService>();
+            s.AddSingleton(new OtherService("Parent"));
+            s.DecorateWithChildServices<IService, DecoratedService>(child => { });
+        });
+
+        var service = provider.GetService<IService>();
+        Assert.NotNull(service);
+        var decoratedService = Assert.IsType<DecoratedService>(service);
+        Assert.IsType<BaseService>(decoratedService.Service);
+        var otherService = Assert.IsType<OtherService>(decoratedService.Other);
+        Assert.Equal("Parent", otherService.Value);
+    }
+
+    private ServiceProvider Build(Action<IServiceCollection> configure)
+    {
+        var services = new ServiceCollection();
+        configure(services);
+        return services.BuildServiceProvider();
+    }
+
+    public class OtherService
+    {
+        public OtherService(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+
+    public interface IService { }
+    public class BaseService : IService { }
+    public class DecoratedService : IService
+    {
+        public DecoratedService(IService service, OtherService other)
+        {
+            Service = service;
+            Other = other;
+        }
+
+        public IService Service { get; }
+        public OtherService Other { get; }
+    }
+}
+
